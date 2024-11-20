3a875cb102e97a510fb92f166f0b7e36fa16afda
yash1216 <yashpandit8631@gmail.com>
2024-11-20 06:05:50 +0530

    mistake

--- src/components/index.js
+++ src/components/index.js
@@ -9,7 +9,7 @@ import Stats from "./Stats";
 import Footer from "./Footer";
 import Testimonials from "./Testimonials";
 import Hero from "./Hero";
-import Services from "./services";
+import Services from "./Services";
 import ContactForm from "./contact";
 import ProjectsSection from "./ProjectsSection";
 
